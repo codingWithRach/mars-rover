3dee9d235f3b3f76e5e233b3a990343e00b199d5
Rachel Miles <rachmiles@gmail.com>
2022-03-11 07:07:42 +0000

    Additional tests for valid and invalid plateaus

--- test/plateau.test.ts
+++ test/plateau.test.ts
@@ -1,7 +1,7 @@
 import { Plateau, isValidPlateau } from "../src/plateau";
 
 describe("isValidPlateau", () => {
-  test.each([[{ x: 1, y: 5 }]])(
+  test.each([[{ x: 1, y: 5 }], [{ x: 0, y: 0 }], [{ x: 0, y: 99999 }]])(
     "returns true if plateau is valid",
     (plateau: Plateau) => {
       expect(isValidPlateau(plateau)).toEqual(true);
@@ -10,7 +10,7 @@ describe("isValidPlateau", () => {
 });
 
 describe("isValidPlateau", () => {
-  test.each([[{ x: -1, y: -5 }], [{ x: 0.4, y: 2 }]])(
+  test.each([[{ x: -1, y: -5 }], [{ x: 0.4, y: 2 }], [{ x: -0.4, y: 42 }]])(
     "returns false if plateau is invalid",
     (plateau: Plateau) => {
       expect(isValidPlateau(plateau)).toEqual(false);
