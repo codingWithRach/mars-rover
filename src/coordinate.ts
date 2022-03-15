8333ca76d05d9ecc8e567a991d3e859663208c8c
Rachel Miles <rachmiles@gmail.com>
2022-03-15 16:33:15 +0000

    Simplify functions in coordinate.ts

--- src/coordinate.ts
+++ src/coordinate.ts
@@ -5,22 +5,9 @@ export interface Coordinate {
 }
 
 export function isValidCoordinate(plateau: Coordinate): boolean {
-  for (const key of Object.keys(plateau)) {
-    if (!Number.isInteger(plateau[key]) || plateau[key] < 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-export function isValidCoordinateArray(plateauArray: Array<number>): boolean {
-  if (plateauArray.length !== 2) return false;
-  for (const dimension of plateauArray) {
-    if (Number.isNaN(dimension)) {
-      return false;
-    }
-  }
-  return true;
+  return ![...Object.values(plateau)].some(
+    (num) => !Number.isInteger(num) || num < 0
+  );
 }
 
 // creates a coordinate from a string in the form '5 5' or '5 5 N'
@@ -29,10 +16,7 @@ export function createCoordinate(coordString: string): Coordinate {
     .split(" ")
     .filter((val) => !Number.isNaN(Number(val)))
     .map((val) => Number(val));
-  if (
-    !isValidCoordinateArray(coordArray) ||
-    !isValidCoordinate({ x: coordArray[0], y: coordArray[1] })
-  )
+  if (!isValidCoordinate({ x: coordArray[0], y: coordArray[1] }))
     throw new Error(ErrorType.ERR_INVALID_COORD);
   return { x: coordArray[0], y: coordArray[1] };
 }
