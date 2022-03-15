89e65ff29e8f240b5894faf5bc2bdd301fa2c92b
Rachel Miles <rachmiles@gmail.com>
2022-03-15 17:51:03 +0000

    Use regex to simplify validation in createCoordinate

--- src/coordinate.ts
+++ src/coordinate.ts
@@ -12,11 +12,16 @@ export function isValidCoordinate(plateau: Coordinate): boolean {
 
 // creates a coordinate from a string in the form '5 5' or '5 5 N'
 export function createCoordinate(coordString: string): Coordinate {
-  const coordArray: Array<number> = coordString
-    .split(" ")
-    .filter((val) => !Number.isNaN(Number(val)))
-    .map((val) => Number(val));
-  if (!isValidCoordinate({ x: coordArray[0], y: coordArray[1] }))
+  if (
+    !coordString.match("[0-9]+ [0-9]+ ?[NSEW]*") ||
+    !isValidCoordinate({
+      x: Number(coordString.split(" ")[0]),
+      y: Number(coordString.split(" ")[1]),
+    })
+  )
     throw new Error(ErrorType.ERR_INVALID_COORD);
-  return { x: coordArray[0], y: coordArray[1] };
+  return {
+    x: Number(coordString.split(" ")[0]),
+    y: Number(coordString.split(" ")[1]),
+  };
 }
