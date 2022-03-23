70134355df391c84b180aa301e65afa2abb2f2b8
Rachel Miles <rachmiles@gmail.com>
2022-03-23 15:35:16 +0000

    Change variable name to coordinate and remove inaccurate comment

--- src/coordinate.ts
+++ src/coordinate.ts
@@ -4,13 +4,13 @@ export interface Coordinate {
   y: number;
 }
 
-export function isValidCoordinate(plateau: Coordinate): boolean {
-  return ![...Object.values(plateau)].some(
+export function isValidCoordinate(coordinate: Coordinate): boolean {
+  return ![...Object.values(coordinate)].some(
     (num) => !Number.isInteger(num) || num < 0
   );
 }
 
-// creates a coordinate from a string in the form '5 5' or '5 5 N'
+// creates a coordinate from a string in the form '5 5'
 export function createCoordinate(coordString: string): Coordinate {
   if (!coordString.match("^[0-9]+ [0-9]+"))
     throw new Error(ErrorType.ERR_INVALID_COORD);
