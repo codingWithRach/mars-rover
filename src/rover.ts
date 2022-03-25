8df4e7a2b4a4a107733e98833f5cc560b80cbcbb
Rachel Miles <rachmiles@gmail.com>
2022-03-25 07:00:10 +0000

    Make private methods private

--- src/rover.ts
+++ src/rover.ts
@@ -13,16 +13,16 @@ export class Rover {
     instructions: string = ""
   ) {
     this.#isDumb = isDumb;
-    this.initPos(startPos);
-    this.initDirection(direction);
+    this.#initPos(startPos);
+    this.#initDirection(direction);
     this.#instructions = instructions.toUpperCase();
   }
 
-  initPos(startPos: Coordinate) {
+  #initPos(startPos: Coordinate) {
     this.#position = new Coordinate(startPos.x, startPos.y);
   }
 
-  initDirection(direction: string = "") {
+  #initDirection(direction: string = "") {
     if (!"NSWE".includes(direction.toUpperCase()) || direction.length !== 1) {
       throw Error(Errors.INVALID_DIR);
     }
